7ee979eb28cf4102d317b2f36fb3ef85bc9d34b8
hicham <hichamelfarissi@Mac.home>
2026-04-14 20:36:42 +0200

    CI.yml adaptation to new build environment

---
+++ nextcloud_data/custom_apps/drawio/l10n/mn.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "drawio",
+    {
+    "File not found." : "Файл олдсонгүй.",
+    "Saving..." : "Хадгалж байна...",
+    "Auto" : "Автомат",
+    "Dark" : "хар",
+    "Yes" : "Тийм",
+    "No" : "Үгүй",
+    "Language" : "Хэл",
+    "Save" : "Хадгалах"
+},
+"nplurals=2; plural=(n != 1);");
---
+++ nextcloud_data/custom_apps/drawio/lib/AppInfo/Application.php
@@ -0,0 +1,135 @@
+<?php
+
+/**
+ *
+ * @author Pawel Rojek <pawel at pawelrojek.com>
+ * @author Ian Reinhart Geiser <igeiser at devonit.com>
+ *
+ * This file is licensed under the Affero General Public License version 3 or later.
+ *
+ **/
+
+namespace OCA\Drawio\AppInfo;
+
+use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\Util;
+use OCP\Files\IMimeTypeDetector;
+use OCP\Files\IMimeTypeLoader;
+
+use OCA\Drawio\AppConfig;
+use OCA\Drawio\Controller\EditorController;
+use OCA\Drawio\Controller\SettingsController;
+use OCA\Drawio\Preview\DrawioPreview;
+use OCA\Drawio\Listeners\FileDeleteListener;
+
+use OCP\EventDispatcher\IEventDispatcher;
+use OCP\Files\Events\Node\NodeDeletedEvent;
+use OCP\Files\IAppData;
+use Psr\Log\LoggerInterface;
+
+
+class Application extends App implements IBootstrap {
+
+    public $appConfig;
+
+    public function __construct(array $urlParams = [])
+    {
+        $appName = "drawio";
+
+        parent::__construct($appName, $urlParams);
+
+        $this->appConfig = new AppConfig($appName);
+    }
+
+    public function register(IRegistrationContext $context): void
+    {
+        $context->registerEventListener(NodeDeletedEvent::class, FileDeleteListener::class);
+
+        $context->registerPreviewProvider(
+            DrawioPreview::class,
+            DrawioPreview::getMimeTypeRegex()
+        );
+    }
+
+    public function boot(IBootContext $context): void
+    {
+        $appName = "drawio";
+
+        // Default script and style if configured
+        if (!empty($this->appConfig->GetDrawioUrl()) && array_key_exists("REQUEST_URI", \OC::$server->getRequest()->server))
+        {
+            $url = \OC::$server->getRequest()->server["REQUEST_URI"];
+
+            if (isset($url)) {
+                if (preg_match("%/apps/files(/.*)?%", $url) || preg_match("%/s/.*%", $url)) // Files app and file sharing
+                {
+                    Util::addScript($appName, "main");
+                    Util::addStyle($appName, "main");
+                }
+            }
+        }
+
+        $container = $context->getAppContainer();
+
+        $container->registerService("L10N", function($c)
+        {
+            return $c->query("ServerContainer")->getL10N($c->query("AppName"));
+        });
+
+        $container->registerService("RootStorage", function($c)
+        {
+            return $c->query("ServerContainer")->getRootFolder();
+        });
+
+        $container->registerService("UserSession", function($c)
+        {
+            return $c->query("ServerContainer")->getUserSession();
+        });
+
+        $container->registerService("Logger", function($c)
+        {
+            return $c->query("ServerContainer")->get(LoggerInterface::class);
+        });
+
+
+        $container->registerService("SettingsController", function($c)
+        {
+            return new SettingsController(
+                $c->query("AppName"),
+                $c->query("Request"),
+                $c->query("L10N"),
+                $c->query("Logger"),
+                $this->appConfig,
+                $c->query(IMimeTypeLoader::class),
+                $c->query(IMimeTypeDetector::class)
+            );
+        });
+
+
+        $container->registerService("EditorController", function($c)
+        {
+            return new EditorController(
+                $c->query("AppName"),
+                $c->query("Request"),
+                $c->query("RootStorage"),
+                $c->query("UserSession"),
+                $c->query("ServerContainer")->getURLGenerator(),
+                $c->query("L10N"),
+                $c->query("Logger"),
+                $this->appConfig,
+                $c->query("IManager"),
+                $c->query("Session"),
+                \OC::$server->getLockingProvider(),
+                \OC::$server->get(IAppData::class)
+            );
+        });
+
+        $detector = $container->query(IMimeTypeDetector::class);
+        $detector->getAllMappings();
+        $detector->registerType("drawio", "application/x-drawio");
+        $detector->registerType("dwb", "application/x-drawio-wb");
+    }
+}
